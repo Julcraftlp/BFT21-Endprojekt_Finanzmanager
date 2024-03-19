cc5e0c367ac1851008758f57fb158d9208c317b4
Kander Akinci <87019962+AI-Assistant@users.noreply.github.com>
2024-03-19 14:19:59 +0000

    Test-AI eingefügt

--- BFT21-Endprojekt_Finanzmanager/Program.cs
+++ BFT21-Endprojekt_Finanzmanager/Program.cs
@@ -2,12 +2,29 @@
 using BFT21_Endprojekt_Finanzmanager.Database.Datasets;
 using Microsoft.EntityFrameworkCore;
 
+
+
 namespace BFT21_Endprojekt_Finanzmanager
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
+            #region Test-AI
+                DatabaseDefiner dbContext = new DatabaseDefiner();
+                
+                dbContext.BankLeitZahl.Add(new BLZ{Nummer=123,Bank="Test"});
+                dbContext.SaveChangesAsync();
+
+                BLZ bank_aktuell = dbContext.BankLeitZahl.FirstOrDefault(k => k.Id == 1); // Angenommen, die ID ist bekannt
+                if (bank_aktuell != null)
+                {
+                    Console.WriteLine(bank_aktuell.Bank);
+                }
+
+            #endregion
+
             //Menue.Init();
             Console.WriteLine("Disclaimer:\nThis software is for demonstrational purposes only\nUse at your own risk, the software does not guarante Anything.\n" +
                 "\nAt any point you can return to the Menue by pressing Escape,\nonly exeption is the Register/Login screen\nIf you do so at any point in this screen the program will terminate Immediately\n" +
